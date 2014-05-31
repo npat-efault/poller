4e5e43d217f7e420cb1993d2659cfab0db5509f8
Nick Patavalis <npat@silver.(none)>
2014-06-01 02:58:47 +0300

    Doc fixes

--- poller.go
+++ poller.go
@@ -57,7 +57,8 @@ func (fdm *fdMap) DelFD(id int) {
 
 // FD is a poller file-descriptor. Typically a file-descriptor
 // connected to a terminal, a pseudo terminal, a character device, a
-// FIFO (named pipe), etc.
+// FIFO (named pipe), of any unix stream that supports the epoll(7)
+// interface.
 type FD struct {
 	id    int // Key in fdMap[]
 	sysfd int // Unix file descriptor
@@ -80,7 +81,8 @@ type FD struct {
 // NewFD initializes and returns a new poller file-descriptor from the
 // given system (unix) file-descriptor. After calling NewFD the system
 // file-descriptor must be used only through the poller.FD methods,
-// not directly.
+// not directly. NewFD sets the system file-descriptor to non-blocking
+// mode.
 func NewFD(sysfd int) (*FD, error) {
 	// Set sysfd to non-blocking mode
 	err := syscall.SetNonblock(sysfd, true)
@@ -173,11 +175,11 @@ func Open(name string, flags int) (*FD, error) {
 
 */
 
-// Read reads up to len(p) bytes into p.  It returns the number of
-// bytes read (0 <= n <= len(p)) and any error encountered. If some
-// data is available but not len(p) bytes, Read returns what is
-// available instead of waiting for more. Read compatible with the
-// Read method of the io.Reader interface. In addition Read honors the
+// Read up to len(p) bytes into p. Returns the number of bytes read (0
+// <= n <= len(p)) and any error encountered. If some data is
+// available but not len(p) bytes, Read returns what is available
+// instead of waiting for more. Read is compatible with the Read
+// method of the io.Reader interface. In addition Read honors the
 // timeout set by (*FD).SetDeadline and (*FD).SetReadDeadline. If no
 // data are read before the timeout expires Read returns with err ==
 // ErrTimeout (and n == 0). If the read(2) system-call returns 0, Read
@@ -270,12 +272,12 @@ func (fd *FD) write1(p []byte) (n int, err error) {
 	return n, err
 }
 
-// Write writes len(p) bytes from p to the file-descriptor.  It
-// returns the number of bytes written (0 <= nn <= len(p)) and any
-// error encountered that caused the write to stop early.  Write
-// returns a non-nil error if it returns nn < len(p). Write is
-// compatible with the Write method of the io.Writer interface. In
-// addition Write honors the timeout set by (*FD).SetDeadline and
+// Writes len(p) bytes from p to the file-descriptor.  Returns the
+// number of bytes written (0 <= nn <= len(p)) and any error
+// encountered that caused the write to stop early.  Write returns a
+// non-nil error if it returns nn < len(p). Write is compatible with
+// the Write method of the io.Writer interface. In addition Write
+// honors the timeout set by (*FD).SetDeadline and
 // (*FD).SetWriteDeadline. If less than len(p) data are writen before
 // the timeout expires Write returns with err == ErrTimeout (and nn <
 // len(p)). If the write(2) system-call returns 0, Write returns with
@@ -283,9 +285,9 @@ func (fd *FD) write1(p []byte) (n int, err error) {
 //
 // Write attempts to write the full amount requested by issuing
 // multiple system calls if required. This sequence of system calls is
-// NOT atomic. If multiple goroutines write to the same FD their
-// Writes may interleave. If you wish to protect agains this, you can
-// embed FD and use a mutex to guard Write method calls:
+// NOT atomic. If multiple goroutines write to the same FD at the same
+// time, their Writes may interleave. If you wish to protect against
+// this, you can embed FD and use a mutex to guard Write method calls:
 //
 //     struct myFD {
 //         poller.FD
@@ -330,11 +332,11 @@ func (fd *FD) Write(p []byte) (nn int, err error) {
 
 */
 
-// Close closes the file descriptor. It is ok to call Close
-// concurently with other operations (Read, Write, SetDeadline, etc)
-// on the same file-descriptor. In this case ongoing operations will
-// return with ErrClosed. Any subsequent operations (after Close) on
-// the file-descriptor will also fail with ErrClosed.
+// Closes the file descriptor. It is ok to call Close concurently with
+// other operations (Read, Write, SetDeadline, etc) on the same
+// file-descriptor. In this case ongoing operations will return with
+// ErrClosed. Any subsequent operations (after Close) on the
+// file-descriptor will also fail with ErrClosed.
 func (fd *FD) Close() error {
 	// Take both locks, to exclude read and write operations from
 	// accessing a closed sysfd.
@@ -372,7 +374,7 @@ func (fd *FD) Close() error {
 
 // SetDeadline sets the deadline for both Read and Write operations on
 // the file-descriptor. Deadlines are expressed as ABSOLUTE instances
-// in time. Example: To set a timeout 5 seconds in the future do:
+// in time. For example, to set a deadline 5 seconds to the future do:
 //
 //   fd.SetDeadline(time.Now().Add(5 * time.Second))
 //
@@ -450,7 +452,7 @@ func (fd *FD) SetWriteDeadline(t time.Time) error {
 	return nil
 }
 
-// Lock locks the file-descriptor. It must be called before perfoming
+// Lock the file-descriptor. It must be called before perfoming
 // miscellaneous operations (e.g. ioctls) on the underlying system
 // file descriptor. It protects the operations against concurent
 // Close's. Typical usage is:
