e3ab4d6536a2c18ef72ad4b46e858f38a9ed6050
Nick Patavalis <npat@efault.net>
2015-11-14 16:23:45 +0200

    Added unsupported.go
    Builds only on non-supported systems.

---
+++ unsupported.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2014, Nick Patavalis (npat@efault.net).
+// All rights reserved.
+// Use of this source code is governed by a BSD-style license that can
+// be found in the LICENSE.txt file.
+
+// +build !linux,!freebsd,!netbsd,!openbsd,!darwin,!dragonfly,!solaris
+
+// Package poller is a file-descriptor multiplexer. This package is
+// not supported on your system.
+package poller
