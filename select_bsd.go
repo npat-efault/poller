56d92cf87c4a657154f2b9d84e5f8fddd6ca06ed
Nick Patavalis <npat@efault.net>
2015-11-19 11:23:33 +0200

    Fixed select for the BSDs
    The Go syscall.Select for the BSD unixes is buggy. It returns only the
    error and not the number of active file descriptors. To cope with
    this, we return "nfd" as the number of active file-descriptors. This
    can cause significant performance degradation but there's nothing else
    we can do.

--- select_bsd.go
+++ select_bsd.go
@@ -10,18 +10,19 @@ package poller
 import "syscall"
 
 func uxSelect(nfd int, r, w, e *fdSet, tv *syscall.Timeval) (n int, err error) {
-	ne := syscall.Select(nfd,
+	// The Go syscall.Select for the BSD unixes is buggy. It
+	// returns only the error and not the number of active file
+	// descriptors. To cope with this, we return "nfd" as the
+	// number of active file-descriptors. This can cause
+	// significant performance degradation but there's nothing
+	// else we can do.
+	err = syscall.Select(nfd,
 		(*syscall.FdSet)(r),
 		(*syscall.FdSet)(w),
 		(*syscall.FdSet)(e),
 		tv)
-
-	var n int
-	if ne != nil {
-		n = int(ne.(syscall.Errno))
+	if err != nil {
+		return 0, err
 	}
-	if n >= 0 {
-		return n, nil
-	}
-	return 0, ne
+	return nfd, nil
 }
